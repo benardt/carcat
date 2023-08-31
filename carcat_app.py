4b4df8046bf08977525300634f1e296d0e918e5c
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 17:04:28 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -2,9 +2,20 @@ import streamlit as st
 import pandas as pd
 
 name = 'Catalogue_Carrick_20230831.xlsx'
-
 df = pd.read_excel('./data/'+name, index_col=0)
+st.write('Nombre total d articles est ', len(df))
 
 number = st.number_input('Insérer le code article', step=1, format="%i")
-st.write('La code article est ', number)
-st.write('Nombre total d articles : ', len(df))
+if st.button('Find'):
+    filtered_df = df.loc[df['code-article']==number]
+    st.write('La code article est ', number)
+    st.write('La prix article est ', filtered_df['prix'])
+    st.write('Le nom  article est ', filtered_df['title'])
+    st.write('Le variant      est ', filtered_df['variant-name'] + " " + filtered_df['variant-option'])
+else:
+    st.write('Press find button')
+
+
+
+
+
