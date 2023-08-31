16086be0d714bd444022f9743dc0a1e35fce78a1
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 17:20:08 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -8,11 +8,7 @@ st.write('Nombre total d articles est ', len(df))
 number = st.number_input('Insérer le code article', step=1, format="%i")
 if st.button('Find'):
     filtered_df = df.loc[df['code-article']==str(number)]
-    st.write('La code article est ', number)
     st.table(filtered_df.T)
-    st.write('La prix article est ', filtered_df['prix'].values[0])
-    st.write('Le nom  article est ', filtered_df['title'].values[0])
-    st.write('Le variant      est ', filtered_df['variant-name'].values[0] + " " + filtered_df['variant-option'].values[0])
 else:
     st.write('Press find button')
 
