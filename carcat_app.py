5d9e44db7e3d095911887582885699986ea3f3af
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 16:30:10 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-number = st.number_input('Insérer le code article', format="%i")
+number = st.number_input('Insérer le code article', step=1, format="%i")
 st.write('La code article est ', number)
