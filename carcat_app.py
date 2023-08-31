c6282b38b326e09d4d90caa4a98e1275e9d8b9c7
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 16:50:18 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 name = 'Catalogue_Carrick_20230831.xlsx'
 
-df = pd.read_excel('/data/'+name, index_col=0)
+df = pd.read_excel('./data/'+name, index_col=0)
 
 number = st.number_input('Insérer le code article', step=1, format="%i")
 st.write('La code article est ', number)
