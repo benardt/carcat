2440db59783798d59c160c4d3fcd6142488443dd
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 17:07:39 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 name = 'Catalogue_Carrick_20230831.xlsx'
 df = pd.read_excel('./data/'+name, index_col=0)
-df = df.astype({'code-article':'int'})
 st.write('Nombre total d articles est ', len(df))
 
 number = st.number_input('Insérer le code article', step=1, format="%i")
 if st.button('Find'):
-    filtered_df = df.loc[df['code-article']==number]
+    filtered_df = df.loc[df['code-article']==str(number)]
     st.write('La code article est ', number)
     st.write('La prix article est ', filtered_df['prix'])
     st.write('Le nom  article est ', filtered_df['title'])
