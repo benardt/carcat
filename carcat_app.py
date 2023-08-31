8579505c498610c5db31650058fe473ce71cfbeb
Thierry Bénard <benardt@users.noreply.github.com>
2023-08-31 16:43:33 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+name = 'Catalogue_Carrick_20230831.xlsx'
+
+df = pd.read_excel('/data/'+name, index_col=0)
 
 number = st.number_input('Insérer le code article', step=1, format="%i")
 st.write('La code article est ', number)
+st.write('Nombre total d'articles : ', len(df))
