4c2219f0aab2ac8cd14f75c8e0def4c9e8e283f4
Thierry Bénard <benardt@users.noreply.github.com>
2023-09-07 15:28:14 +0200

    Update carcat_app.py

--- carcat_app.py
+++ carcat_app.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-name = 'Catalogue_Carrick_20230905.xlsx'
-df = pd.read_excel('./data/'+name, index_col=0)
-st.write('Nombre total d articles est ', len(df))
+def main():
+    name = 'Catalogue_Carrick_20230905.xlsx'
+    df = pd.read_excel('./data/'+name, index_col=0)
+    st.write('Nombre total d articles est ', len(df))
+    
+    number = st.number_input('Insérer le code article', step=1, format="%i")
+    if st.button('Find'):
+        filtered_df = df.loc[df['code-article']==str(number)]
+        st.table(filtered_df.T)
+    else:
+        st.write('Press find button')
 
-number = st.number_input('Insérer le code article', step=1, format="%i")
-if st.button('Find'):
-    filtered_df = df.loc[df['code-article']==str(number)]
-    st.table(filtered_df.T)
-else:
-    st.write('Press find button')
-
-
-
-
-
+if __name__ == '__main__':
+    main()
